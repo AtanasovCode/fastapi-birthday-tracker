dd0bea5e4f499acb351ea9b8c30f8ab104de75b1
AtanasovCode <a_ljupce@yahoo.com>
2026-05-20 01:03:48 +0200

    create API router & create GET endpoint

---
+++ web/person_router.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+from fastapi.params import Depends
+from sqlalchemy.orm import Session
+from database.database import get_db
+from model.schema import PersonSchema
+from service import person_service as people, category_service as categories
+
+
+router = APIRouter(prefix="/birthdays", tags=["People"])
+
+@router.get("/", response_model=list[PersonSchema])
+def list_all(db: Session = Depends(get_db)):
+    return people.list_all(db)
