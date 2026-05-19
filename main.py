c62f5b98ad359ffaba2f68fa73a5edfc03da4ce6
AtanasovCode <a_ljupce@yahoo.com>
2026-05-20 01:22:50 +0200

    seed database & add new person router

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from web.person_router import router as person_api_router
+from database.seed import seed
+from database.database import engine
+from model.models import Base
 
-app = FastAPI()
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(bind=engine)
+    seed()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(person_api_router)
 
 @app.get("/")
 async def root():
